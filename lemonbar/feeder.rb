198d746a792a7d88437b9c8d8ec239b2cc8b620c
4Kamei <kamei@gmail.com>
2017-06-09 21:01:24 +0100

    lemonbar working and printing messages

--- lemonbar/feeder.rb
+++ lemonbar/feeder.rb
@@ -1,40 +1,47 @@
 require 'i3ipc'
 require 'listen'
 
+def message (m)
+  $message = m
+  $pipe.puts "%{l}>>#{$message} %{r}#{Time.now}"
+end
+name_map = {}
+focused = ""
+$message = ""
+
 listener = Listen.to("..") do |mod, add, rem|
   str = mod[0][-10..-1]
   if str == "theme_info"
-    puts "UPDATING COLOURS"
   end
 end
-
-begin
 listener.start
-rescue
-  puts "test"
-end
-name_map = {}
-focused =
 
 i3 = I3Ipc::Connection.new
 work = i3.workspaces
 
-
 work.each { |e|
-  puts e
   name_map[e.num] = e.name[2..-1]
 }
 
-name_map.each { |key, value| puts "workspace #{key} has name #{value}"}
-
 block = Proc.new do |reply|
   if reply.change == "focus"
-    focused = reply.current.num
-
+    message reply.current.name
   else
     puts "Unknown change #{reply.change}"
   end
 end
 
 pid = i3.subscribe('workspace', block)
-pid.join
+i3List = Thread.new{pid.join()}
+Signal.trap("PIPE", "EXIT")
+
+$pipe = IO.popen("sh bar", "r+")
+
+last = Time.now
+while true
+    $pipe.puts "%{l}>>#{$message} %{r}#{Time.now}"
+    now = Time.now
+    _next = [last + 1,now].max
+    sleep (_next-now)
+    last = _next
+end
